dc4e79876d33ef3c395314b1977c42e62df9ec12
LMDM-LR <71432123+LMDM-LR@users.noreply.github.com>
2020-09-24 10:52:57 +0200

    Add files via upload

--- mainjs.js
+++ mainjs.js
@@ -2,3 +2,7 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
   });
+
+$(document).on('click', '[id="formbtn"]', function(event) {
+  event.window.open("index.html")
+});
